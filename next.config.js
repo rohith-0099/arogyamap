54707c5e8180dfca3e1f233eb7e4943bfd5e5428
Rohith <rohithchandran0011@gmail.com>
2026-04-13 15:04:27 +0530

    config: add next.js configuration

---
+++ next.config.js
@@ -0,0 +1,33 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "offlineCache",
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["xmfotuoxwqnobnimqklf.supabase.co"],
+  },
+};
+
+module.exports = withPWA(nextConfig);
